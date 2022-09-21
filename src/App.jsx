78a5595c9758021eed2ca9e5e141ca3de54f4b57
khjoo1203 <khjoo1203@naver.com>
2022-09-21 12:59:41 +0900

    back to the list when searching

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,41 @@ import styles from './App.module.css';
 import { useState, useEffect } from 'react';
 import VideoList from './components/video_list/VideoList';
 import SearchHeader from './components/search_header/SearchHeader';
+import VideoDetail from './components/video_detail/VideoDetail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
   const search = (query) => {
     youtube
       .search(query) //
-      .then(videos => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos)
+        setSelectedVideo(null);
+      });
   };
   useEffect(() => {
     youtube
       .mostPopular() //
-      .then(videos => setVideos(videos));
+      .then((videos) => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo? 'list' : 'grid'} />
+        </div>
+      </section>
     </div>
   );
 }
